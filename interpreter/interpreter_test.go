1fa32322696470d379039562a2a657bd1f91215e
Richard Wilkes <rwilkes@fanatics.com>
2017-05-18 07:30:10 -0700

    Consolidate

--- interpreter/interpreter_test.go
+++ interpreter/interpreter_test.go
@@ -3,12 +3,16 @@ package interpreter_test
 import (
 	"fmt"
 	"reflect"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/richardwilkes/goblin"
+	"github.com/richardwilkes/goblin/interpreter"
 	"github.com/richardwilkes/goblin/interpreter/parser"
 	"github.com/richardwilkes/goblin/util"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestComments(t *testing.T) {
@@ -348,4 +352,26 @@ func checkDeclaration(t *testing.T, script string, expected interface{}) {
 	if assert.NoError(t, err, script) {
 		assert.True(t, util.Equal(reflect.ValueOf(expected), v), "script: %s\nexpected: %v\nreceived: %v", script, expected, v)
 	}
+}
+
+func TestInterrupt(t *testing.T) {
+	stmts, err := parser.Parse(`sleep("5s"); println("This should not be printed")`)
+	require.NoError(t, err)
+	var wg sync.WaitGroup
+	for i := 0; i < 30; i++ {
+		wg.Add(1)
+		go testInterrupt(t, stmts, &wg)
+	}
+	wg.Wait()
+}
+
+func testInterrupt(t *testing.T, stmts []interpreter.Stmt, wg *sync.WaitGroup) {
+	defer wg.Done()
+	env := interpreter.NewEnv()
+	go func() {
+		time.Sleep(time.Second)
+		env.Interrupt()
+	}()
+	_, err := env.Run(stmts)
+	assert.Equal(t, interpreter.ErrInterrupt, err)
 }
